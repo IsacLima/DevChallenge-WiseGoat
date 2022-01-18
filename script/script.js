2313a377c0a6858bc0b2ec823e5683cb9cd6191f
Isaque Lima Leite <30935061+IsacLima@users.noreply.github.com>
2022-01-18 06:09:11 -0300

    Update script.js
    comentando para futuras consultas.

--- script/script.js
+++ script/script.js
@@ -1,17 +1,27 @@
+//link da API que consumiremos
 const url = 'https://api.adviceslip.com/advice';
 
+//funcao que puxa os conselhos
 function fazGet(u){
+	//crio um arquivo requirindo o conteudo
 	let request = new XMLHttpRequest();
 	request.open("GET", u, false);
+	//o send() esta vazio porque nao to eviando nada, somente recebendo
 	request.send();
+	//salva o conteudo recebido em texto
 	return request.responseText
 }
 
 function main(){
+	//chamo a função
 	let data = fazGet(url);
+	//transformo o texto retornado em .JSON
 	let conselho = JSON.parse(data);
+	//aqui abro o documento onde o arquivo .JSON vai colocar o texto
 	let texto = document.getElementById("texto");
+	//aqui pego o conselho e salvo numa variavel
 	let conteudo =conselho.slip.advice;
+	//aqui adiciono aspas ao conselho e mando como conteudo para o html
 	texto.innerHTML = " ''" + conteudo + " ''";
 }
 
